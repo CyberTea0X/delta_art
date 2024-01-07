3bb5e923086a636cfe7d7fc7018d2883c4b7dcc2
CyberTea0X <mihailusov_778@mail.ru>
2024-01-07 19:10:46 +0300

    Redisign of main page and refactoring of image loading

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,86 +1,50 @@
-import styles from './page.module.css'
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import Image from 'next/image';
-import catBlack from '../../public/homepage/cat-black.png';
-import catBlue from '../../public/homepage/cat-blue.png';
-import catPixel from '../../public/homepage/cat-pixel.png';
-import catSpace from '../../public/homepage/cat-space.png';
-import catWhite from '../../public/homepage/cat-white.png';
-import dragonCat from '../../public/homepage/dragon-cat.png';
-import girl from '../../public/homepage/girl.png';
-import lizardPurple from '../../public/homepage/lizard-purple.png';
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
+import { Typography } from '@mui/material';
 
 export default function Home() {
 
-  const t  = useTranslations('home');
+  const t = useTranslations('home');
   return (
-    <main className={styles.main}>
-    <center>
-    <h1> 
-      {t("greeting")}
-    </h1>
-    <p>
-      {t("slogan")}
-    </p>
-    <h2>
-      {t("slogan2")}
-    </h2>
-    <Box sx={{ width: 1200, height: 600, overflowy: 'scroll'}}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant="h3" fontWeight={500}>
+        {t("greeting")}
+      </Typography>
+      <Typography variant="h4">
+        {t("slogan")}
+      </Typography>
+      <Typography variant="h5">
+        {t("slogan2")}
+      </Typography>
       <ImageList variant="masonry" cols={4} gap={8}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.title}>
-            <Image
-              src={item.img}
-              alt={item.title}
+        {itemData.map((src) => (
+          <ImageListItem key={src}>
+            {/*Cannot use Image component because of this issue: https://github.com/mui/material-ui/issues/36820*/}
+            <img
+              srcSet={`${src}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${src}?w=248&fit=crop&auto=format`}
+              alt={"Image of some author"}
               loading="lazy"
-              width={300}
             />
           </ImageListItem>
         ))}
       </ImageList>
     </Box>
-    </center>
-    </main>
   )
 }
 
 
-// Yes, I'm just importing them here by hand, this is because the images are for testing purposes only.
 const itemData = [
-  {
-    img: lizardPurple,
-    title: 'lizardPurple'
-  },
-  {
-    img: girl,
-    title: 'girl'
-  },
-  {
-    img: dragonCat,
-    title: 'dragonCat'
-  },
-  {
-    img: catWhite,
-    title: 'catWhite'
-  },
-  {
-    img: catSpace,
-    title: 'catSpace'
-  },
-  {
-    img: catPixel,
-    title: 'catPixel'
-  },
-  {
-    img: catBlue,
-    title: 'catBlue'
-  },
-  {
-    img: catBlack,
-    title: 'catBlack'
-  }
+  '/homepage/cat-black.png',
+  '/homepage/cat-blue.png',
+  '/homepage/cat-pixel.png',
+  '/homepage/cat-space.png',
+  '/homepage/cat-white.png',
+  '/homepage/dragon-cat.png',
+  '/homepage/girl.png',
+  '/homepage/lizard-purple.png',
+  '/homepage/octocat.png',
 ];
